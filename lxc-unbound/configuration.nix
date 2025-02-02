da4424302ea7d1cbdc538c5a16da76db1b9f29ea
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2025-02-02 10:59:53 +0100

    add local data

--- lxc-unbound/configuration.nix
+++ lxc-unbound/configuration.nix
@@ -58,7 +58,9 @@
         access-control = "192.168.0.0/16 allow";
         private-domain = "lan.d35c.net";
         local-zone = "\"lan.d35c.net.\" static";
-        local-data = "home.lan.d35c.net. IN A 192.168.10.1";
+        local-data = "\"jellyfin.lan.d35c.net.\tIN A 192.168.10.11\"";
+        local-data = "\"shiori.lan.d35c.net.\tIN A 192.168.10.11\"";
+        local-data = "\"bucket.lan.d35c.net.\tIN A 192.168.10.11\"";
       };
 
       forward-zone = [
